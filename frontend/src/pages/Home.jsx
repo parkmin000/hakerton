4898cb61260b34a94fd82baa71388560d0cde9a5
min000 <min0@abmlab.co.kr>
2026-03-19 21:16:24 +0900

    졸륌

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,28 +4,31 @@ import './pages.css'
 function Home() {
   return (
     <section id="center">
-      <div>
+      <div className="home-hero">
+        <h1 className="home-title">최선을 다했습니다</h1>
+        <p className="home-subtitle">뭘 좋아할지 몰라서 다 준비한 팀</p>
+
         <div className="home-actions">
-          <NavLink className="home-link" to="/minyoung">
-            맥주 따르기
+          <NavLink className="home-link" to="/minyoung/alcohol/beer">
+            진성 술게임, 맥주 따르기
           </NavLink>
           <NavLink className="home-link" to="/minyoung/alcohol/map">
-            지도 게임
+            캠퍼스 속, 미루 하루를 찾아라
           </NavLink>
           <NavLink className="home-link" to="/yeonhee/stack">
-            블록쌓기
+            블록 쌓기 게임
           </NavLink>
           <NavLink className="home-link" to="/games/guess/jieun2">
-            테트리스
+            차곡차곡 시간표 테트리스
           </NavLink>
           <NavLink className="home-link" to="/minyoung/alcohol/mouse">
-            피하기
+            요리조리 미루 하루 피하기
           </NavLink>
           <NavLink className="home-link" to="/jieun/credit">
-            학점받기
+            학점 올리기 게임
           </NavLink>
           <NavLink className="home-link" to="/yeonhee/mole">
-            두더지잡기
+            하루 내리고, 미루 올려
           </NavLink>
         </div>
       </div>
